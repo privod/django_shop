423b385281072c292712158a0eb729c718cd0229
privod <privod.org@gmail.com>
2018-04-01 22:38:26 +0700

    Заданы функции возвращающие ссылку на товар и категорию

--- project/myshop/shop/models.py
+++ project/myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 # Модель категории
@@ -14,10 +15,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
 
 # Модель продукта
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', verbose_name=Category.Meta.verbose_name)
+    category = models.ForeignKey(Category, related_name='products', verbose_name='Категория')   # TODO: HARDCODE!!!
     name = models.CharField(max_length=200, db_index=True, verbose_name="Название")
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, verbose_name="Изображение товара")
@@ -36,3 +40,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
--- project/myshop/shop/urls.py
+++ project/myshop/shop/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='ProductListByCategory'),
-    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='ProductDetail'),
-    url(r'^$', views.product_list, name='ProductList'),
+    url(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
+    url(r'^$', views.product_list, name='product_list'),
 ]
